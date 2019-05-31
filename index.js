4929af2f1b331dd3d17156a9380728e342226f5e
Christian Duncan <christianeduncan3@gmail.com>
2019-05-31 15:55:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Spongebob';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
